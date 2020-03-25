a56a127dc07d82b195edcaa18c1ab852c8b78efe
Snow23cumo <yonny.cuesta2017@unaula.edu.co>
2020-03-25 16:44:43 -0500

    Trabajo con Seeder y ModelFactory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,14 +3,31 @@
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
     {
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run()
+        {
+        $this->truncateTablas([
+            'rol',
+            'permiso'
+            
+        ]);
+
         // $this->call(UsersTableSeeder::class);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
+    }
+
+    protected function truncateTablas(array $tablas){
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); //deshabilitar las llaves foraneas
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); //habilitar las llaves foraneas
     }
 }
